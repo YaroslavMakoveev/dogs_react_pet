50138bfb1febf45e43f11c29066d3b651d2dd526
YaroslavMakoveev <y-makoveev@mail.ru>
2025-09-17 20:15:17 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import FavoritesPage from "./pages/FavoritesPage";
 function App() {
   return (
-    <Router basename="/dogs_react_pet">
+    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
