75dee9f59f21f358bf62d454748f0f16bdedd4a6
YaroslavMakoveev <y-makoveev@mail.ru>
2025-09-17 21:56:25 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import FavoritesPage from "./pages/FavoritesPage";
 function App() {
